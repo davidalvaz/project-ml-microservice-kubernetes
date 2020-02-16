9e7d27804086f9c78fd3fe0126496f47a486c981
David Alvarez <david.alvarezz@gmail.com>
2020-02-16 13:45:12 -0300

    feat: Deploy Docker Hub container with kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="dalvaz/ml-microservice-api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment ml-microservice-api --image="$dockerpath":1.0
+kubectl expose deployment/ml-microservice-api --port 80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/ml-microservice-api 8000:80
 
