e348c27abb80b4ccc14919390a9045b3b95c3ab9
David Alvarez <david.alvarezz@gmail.com>
2020-02-16 20:18:42 -0300

    fix: Create typeNode and port service

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -7,7 +7,7 @@ dockerpath="dalvaz/ml-microservice-api"
 # Step 2
 # Run the Docker Hub container with kubernetes
 kubectl create deployment ml-microservice-api --image="$dockerpath":1.0
-kubectl expose deployment/ml-microservice-api --port 80
+kubectl expose deployment/ml-microservice-api --type=NodePort --port=80 --target-port=80
 
 # Step 3:
 # List kubernetes pods
